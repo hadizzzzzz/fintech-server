264507fa883d379584b6b5efb3196575cb368184
yminjuu <ymj0580@naver.com>
2024-08-13 15:39:07 +0900

    feat: security 설정 및 공통 DTO 생성

---
+++ demo/src/main/java/demo/demo/bank/config/SecurityConfig.java
@@ -0,0 +1,103 @@
+package demo.demo.bank.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import demo.demo.bank.dto.ResponseDto;
+import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+//import shop.mtcoding.bank.config.jwt.JwtAuthenticationFilter;
+//import shop.mtcoding.bank.config.jwt.JwtAuthorizationFilter;
+import demo.demo.bank.domain.user.UserEnum;
+import demo.demo.bank.util.CustomResponseUtil;
+
+@Configuration
+public class SecurityConfig {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Bean // Ioc 컨테이너에 BCryptPasswordEncoder() 객체가 등록됨.
+    public BCryptPasswordEncoder passwordEncoder() {
+        log.debug("디버그 : BCryptPasswordEncoder 빈 등록됨");
+        return new BCryptPasswordEncoder();
+    }
+
+    // JWT 필터 등록이 필요함 : 임시 주석 처리
+//    public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
+//        @Override
+//        public void configure(HttpSecurity builder) throws Exception {
+//            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+//            builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
+//            builder.addFilter(new JwtAuthorizationFilter(authenticationManager));
+//            super.configure(builder);
+//        }
+//    }
+
+    // JWT 서버를 만들 예정!! Session 사용안함.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.debug("디버그 : filterChain 빈 등록됨");
+        http.headers().frameOptions().sameOrigin(); // iframe 허용안함.
+        http.csrf().disable(); // enable이면 post맨 작동안함
+        http.cors().configurationSource(configurationSource());
+
+        // jSessionId를 서버쪽에서 관리안하겠다는 뜻!!
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // react, 앱으로 요청할 예정
+        http.formLogin().disable();
+        // httpBasic은 브라우저가 팝업창을 이용해서 사용자 인증을 진행한다.
+        http.httpBasic().disable();
+
+        // Exception 가로 채기
+        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+            String uri = request.getRequestURI();
+            log.debug("디버그: "+ uri);
+            CustomResponseUtil.unAuthentication(response, "로그인을 진행해주세요");
+        });
+
+        // 임시 주석 처리
+//        // 필터 적용
+//        http.apply(new CustomSecurityFilterManager());
+//
+//        // 인증 실패
+//        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+//            CustomResponseUtil.fail(response, "로그인을 진행해 주세요", HttpStatus.UNAUTHORIZED);
+//        });
+//
+//        // 권한 실패
+//        http.exceptionHandling().accessDeniedHandler((request, response, e) -> {
+//            CustomResponseUtil.fail(response, "권한이 없습니다", HttpStatus.FORBIDDEN);
+//        });
+
+        // https://docs.spring.io/spring-security/reference/servlet/authorization/authorize-http-requests.html
+        http.authorizeRequests()
+                .antMatchers("/api/s/**").authenticated()
+                .antMatchers("/api/admin/**").hasRole("" + UserEnum.ADMIN) // 최근 공식문서에서는 ROLE_ 안붙여도 됨
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+
+    public CorsConfigurationSource configurationSource() {
+        log.debug("디버그 : configurationSource cors 설정이 SecurityFilterChain에 등록됨");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트 앤드 IP만 허용 react)
+        configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+        configuration.addExposedHeader("Authorization"); // 옛날에는 디폴트 였다. 지금은 아닙니다.
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
